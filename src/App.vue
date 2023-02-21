3ef039507c3e6d33f229026128889a5796142cde
Mark Hahn <mark@hahnca.com>
2023-02-20 22:08:53 -0800

    fixed layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template lang="pug">
-div(style=`width: 100%; display: flex;
+div(style=`width: 100vw; display: flex;
            flex-direction: column;`)
-  #hdr(style=`border: 2px solid black; width: 100%;
-              position: fixed; left: 0; top: 0;
+  #hdr(style=`width: 100vw; border-radius: .5em;
               background-color: yellow;
-              flex-basis: 2.5em;`)
-    div(style=`padding: 0 1em; display: flex;`)
-      div(style=`font-weight: bold; font-size: 1.5em;
-                  flex-basis: 2.5em; margin-top: .25em;`) SSJ
-      input(v-model="topExpr" @input="topExprInput"
-            style=`border: 2px solid black; height: 1.5em;
-                   font-size: 1.2em; flex-grow: 2; margin: .25em 0;`)
-  #desk(style=`flex-grow: 2;
-               position: fixed; left: 0; top: 2.8em;
-               border: 2px solid black; width: 100%;`)
+              flex-basis: 2em; display: flex;`)
+    span(style=`font-weight: bold; font-size: 1.5em;
+                flex-basis: 2em; margin: .25em 0 0 .5em;`) SSJ
+    input(v-model="topExpr" @input="topExprInput"
+          style=`border: 2px solid black; 
+                 height: 1.5em; margin: .25em 0;
+                 font-size: 1em; flex-grow: 1;`)
+  #desk(style=`width: 100vw; flex-grow: 1;`) hello world 
 </template>
 
 <script>
